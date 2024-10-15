9ab70f9c93756aadbc6a3f27efd08d938aa3ee97
Lucas Gabriel <138629420+luquasgabriel@users.noreply.github.com>
2024-10-14 23:53:07 -0300

    Correção na função contains()

--- set_with_queue.py
+++ set_with_queue.py
@@ -44,17 +44,18 @@ class SetWithQueue:
 
     def contains(self, elemento):
         fila_temp = FilaArray()
+        encontrado = False
 
         while len(self._fila_dados) > 0:
             atual = self._fila_dados.dequeue()
             fila_temp.enqueue(atual)
             if atual == elemento:
-                return True
+                encontrado = True
 
         while len(fila_temp) > 0:
             self._fila_dados.enqueue(fila_temp.dequeue())
 
-        return False
+        return encontrado
 
     def __str__(self):
         return f"SetWithQueue({self.list()})"
